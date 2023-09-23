35e18132a0c684597f33c0110f90f6c5ca260cdf
Fadel Muhammad <114661628+Dewalion22@users.noreply.github.com>
2023-09-23 08:52:18 +0700

    Update firstLinked.js

--- Linked list/firstLinked.js
+++ Linked list/firstLinked.js
@@ -1,16 +1,34 @@
-// 10 --->5--->16
+// Create the below linked list:
+// myLinkedList = {
+//   head: {
+//     value: 10
+//     next: {
+//       value: 5
+//       next: {
+//         value: 16
+//         next: null
+//       }
+//     }
+//   }
+// };
 
-class LinkedList  {
-    constructor(value){
+class LinkedList {
+  constructor(value) {
     this.head = {
-        value : value,
-        next : null
-    }
+      value: value,
+      next: null
+    };
     this.tail = this.head;
     this.length = 1;
-
-    }
+  }
+  append(value) {
+    //Code here
+  }
 }
 
-const myLinkedList1 = new LinkedList(10);
-console.log(myLinkedList1);
+let myLinkedList = new LinkedList(10);
+myLinkedList.append(5);
+myLinkedList.append(16);
+
+
+
